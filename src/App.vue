bc6fa5458e389c7011458e647417e1ff818f45e9
oleg086rus <o.pototskiy@yandex.ru>
2023-01-09 23:36:05 +0500

    auth start

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <router-view/>
+  <component :is="layout + '-layout'" v-if="layout"/>
 </template>
+
+<script>
+import MainLayout from '@/layout/MainLayout.vue'
+import AuthLayout from '@/layout/AuthLayout.vue'
+import { useRoute } from 'vue-router/dist/vue-router'
+import { computed } from 'vue'
+
+export default {
+  setup() {
+    const route = useRoute()
+
+    return {
+      layout: computed(() => route.meta.layout)
+    }
+  },
+  components: {MainLayout, AuthLayout}
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import Auth from '@/views/Auth.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      layout: 'main'
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    path: '/auth',
+    name: 'Auth',
+    component: Auth,
+    meta: {
+      layout: 'auth'
+    }
+  },
+  {
+    path: '/help',
+    name: 'Help',
+    component: () => import('../views/Help.vue'),
+    meta: {
+      layout: 'main'
+    }
   }
 ]
 

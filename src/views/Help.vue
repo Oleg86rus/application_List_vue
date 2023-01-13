08c7b8cbb36c17993f009cb864e239d9f3666f10
oleg086rus <o.pototskiy@yandex.ru>
2023-01-13 12:05:35 +0500

    help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Помощь">
+    <question-item
+        v-for="q in questions"
+        :key="q.title"
+        :question="q"
+    >
+    </question-item>
+  </app-page>
+
 </template>
+
+<script>
+
+import AppPage from '@/components/ui/AppPage.vue'
+import QuestionItem from '@/components/question/QuestionItem.vue'
+
+export default {
+  components: {AppPage, QuestionItem},
+  setup() {
+    const questions = [
+      {
+        title: 'Вопрос 1',
+        text: 'ответ 1'
+      },
+      {
+        title: 'Вопрос 2',
+        text: 'ответ 2'
+      }
+    ]
+    return {questions}
+  }
+}
+</script>

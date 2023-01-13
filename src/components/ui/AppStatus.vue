43cd15e0154d87e7d7d8276151263ef03614b45e
oleg086rus <o.pototskiy@yandex.ru>
2023-01-13 11:52:28 +0500

    edit and delete tasks

--- src/components/ui/AppStatus.vue
+++ src/components/ui/AppStatus.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 export default {
   name: 'AppStatus',
@@ -27,6 +27,12 @@ export default {
       done: 'Завершен',
       pending: 'Выполняется'
     }
+
+    watch(props, val => {
+      className.value = classesMap[val.type]
+      text.value = textMap[val.type]
+    })
+
     const className = ref(classesMap[props.type])
     const text = ref(textMap[props.type])
     return {
--- src/views/Request.vue
+++ src/views/Request.vue
@@ -5,6 +5,19 @@
     <p><strong>Телефон</strong>: {{request.phone}}</p>
     <p><strong>Сумма</strong>: {{currency(request.amount)}}</p>
     <p><strong>Статус</strong>: <app-status :type="request.status"/></p>
+
+    <div class="form-control">
+      <label for="status">Статус</label>
+      <select id="status" v-model="status">
+        <option value="done">Завершен</option>
+        <option value="cancelled">Отменен</option>
+        <option value="active">Активен</option>
+        <option value="pending">Выполняется</option>
+      </select>
+    </div>
+
+    <button class="btn danger" @click="remove">Удалить</button>
+    <button class="btn" v-if="hasChanges" @click="update">Обновить</button>
   </app-page>
   <h3 v-else class="text-center text-white">
     Заявки с ID = {{ route.params.id }} нет
@@ -17,9 +30,10 @@ import AppPage from '@/components/ui/AppPage.vue'
 import AppLoader from '@/components/ui/AppLoader.vue'
 import AppStatus from '@/components/ui/AppStatus.vue'
 import { useRoute } from 'vue-router'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed, markRaw } from 'vue'
 import { useStore } from 'vuex'
 import { currency } from '@/utils/currency'
+import { useRouter } from 'vue-router/dist/vue-router'
 
 export default {
   name: 'Request',
@@ -27,16 +41,32 @@ export default {
   setup() {
     const route = useRoute()
     const store = useStore()
+    const router = useRouter()
     const loading = ref(true)
     const request = ref({})
+    const status = ref()
 
     onMounted(async () => {
       loading.value = true
       request.value = await store.dispatch('request/loadById', route.params.id)
+      status.value = request.value?.status
       loading.value = false
     })
 
-    return {request, loading, route, currency}
+    const hasChanges = computed(() => request.value.status !== status.value)
+
+    const remove = async () => {
+      await store.dispatch('request/remove', route.params.id)
+      router.push('/')
+    }
+
+    const update = async () => {
+      const data = {...request.value, id: route.params.id, status: status.value}
+      await store.dispatch('request/update', data)
+      request.value.status = status.value
+    }
+
+    return {request, loading, route, currency, remove, update, status, hasChanges}
   }
 }
 </script>
